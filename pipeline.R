451a2d5ac9ed473f158cc10e381c69278f3473fe
fangchiliu <jliu3933@gmail.com>
2026-05-13 17:15:29 -0700

    Create pipeline.R

---
+++ pipeline.R
@@ -0,0 +1,23 @@
+# Project setup
+getwd()
+
+
+plots_dir = file.path(getwd(),"plots")
+dir.create(plots_dir, showWarnings = FALSE, recursive = TRUE)
+
+plots_dir = file.path(getwd(),"exports")
+dir.create(plots_dir, 
+           showWarnings = FALSE, 
+           recursive = TRUE)
+
+list.files()
+list.dirs(getwd(), recursive = FALSE)
+
+
+
+
+
+
+
+
+
